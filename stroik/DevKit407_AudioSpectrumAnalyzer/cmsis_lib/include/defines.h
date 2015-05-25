15d813d37de047f2b66b45f077cb80f96e18c253
michal93cz <michal.czupryniak@gmail.com>
2015-05-25 21:16:02 +0200

    zmiana defines

--- stroik/DevKit407_AudioSpectrumAnalyzer/cmsis_lib/include/defines.h
+++ stroik/DevKit407_AudioSpectrumAnalyzer/cmsis_lib/include/defines.h
@@ -11,5 +11,7 @@
 #define TM_DEFINES_H
 
 //Put your global defines for all libraries here used in your project
+#define PCD8544_SPI				SPI3
+#define PCD8544_SPI_PINSPACK 	TM_SPI_PinsPack_1
 
 #endif
